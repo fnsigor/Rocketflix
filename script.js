3f04275e808c61a8ffa36d8dd93b321e6e5f518b
Igor Fernandes <igorfernamdez@gmail.com>
2022-11-26 22:31:11 -0300

    add loading animation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const getRandomNumber = () => Math.floor(Math.random() * 9000)
 
 
-function getFormatedReleaseDate(apiDate){
+function getFormatedReleaseDate(apiDate) {
     const date = apiDate.split('-')
     const year = date[0]
     const mounth = date[1]
@@ -11,56 +11,61 @@ function getFormatedReleaseDate(apiDate){
 }
 
 
-function removeLastCaractere(string){
-    string =  string.split(',')
+function updateLoadingStatus(){
+    document.getElementById('loadingIcon').classList.toggle('hide')
+    document.querySelector('main').classList.toggle('hide')
+}
+
+
+function removeLastCaractere(string) {
+    string = string.split(',')
     string.pop()
     return string.join(', ')
 }
 
 
-function setMovieSecondaryInfo(object, getFormatedReleaseDate, removeLastCaractere){
+async function setMoviePoster(path) {
+    try {
+        const response = await axios.get(`https://image.tmdb.org/t/p/w500/${path}`)
+        document.getElementById('movie-poster').style.backgroundImage = `url(https://image.tmdb.org/t/p/w500/${response.request.responseURL})`
+    } catch (error) {
+        document.getElementById('movie-poster').style.backgroundImage = `url(assets/interrogacao.jpg)`
+    }
+    updateLoadingStatus()
+
+}
+
+function setMovieInfo(data) {
+
     let genresSpan = ''
     let productionCountries = ''
 
-    for(let genre of object.genres){
+    for (let genre of data.genres) {
         genresSpan += `<span class="api-span">${genre.name}</span>, `
     }
 
 
-    for(let country of object.production_countries){
+    for (let country of data.production_countries) {
         productionCountries += `<span class="api-span">${country.name}</span>, `
     }
 
     document.getElementById('li-genres').innerHTML = `<span class="span-title">Genres:</span> ${removeLastCaractere(genresSpan)}`
-    document.getElementById('original-title').innerHTML = `<span class="span-title">Original title:</span> <span class="api-span">${object.original_title}</span>`
-    document.getElementById('release-date').innerHTML = `<span class="span-title">Release date:</span> <span class="api-span">${getFormatedReleaseDate(object.release_date)}</span>`
+    document.getElementById('original-title').innerHTML = `<span class="span-title">Original title:</span> <span class="api-span">${data.original_title}</span>`
+    document.getElementById('release-date').innerHTML = `<span class="span-title">Release date:</span> <span class="api-span">${getFormatedReleaseDate(data.release_date)}</span>`
     document.getElementById('production-countries').innerHTML = `<span class="span-title">Production countries:</span> ${removeLastCaractere(productionCountries)}`
-}
 
-async function setMoviePoster(path){
-    try {
-         const response = await axios.get(`https://image.tmdb.org/t/p/w500/${path}`)
-         document.getElementById('movie-poster').style.backgroundImage = `url(https://image.tmdb.org/t/p/w500/${response.request.responseURL})`
-    } catch (error) {
-        document.getElementById('movie-poster').style.backgroundImage = `url(assets/interrogacao.jpg)`
-    }
-    
-}
+    document.getElementById('movie-title').innerText = data.title
+    document.getElementById('movie-description').innerText = data.overview
 
-function setMovieMainInfo(title, description, posterPath, object, getFormatedReleaseDate, removeLastCaractere, setMoviePoster){
     
-    document.getElementById('movie-title').innerText = title
-    document.getElementById('movie-description').innerText = description
-    setMoviePoster(posterPath)
-    setMovieSecondaryInfo(object, getFormatedReleaseDate, removeLastCaractere)
+    setMoviePoster(data.poster_path)
 }
 
 
 async function searchRandomMovie() {
     try {
-        //getRandomNumber está sendo buscada no escopo global, contra os padrões do paradigma funcional, onde as funcoes devem passadas como parametro
         const json = await axios.get(`https://api.themoviedb.org/3/movie/${getRandomNumber()}?api_key=7bd52ba876f696e68d8505e5fa820bc1`)
-        setMovieMainInfo(json.data.title, json.data.overview, json.data.poster_path, json.data, getFormatedReleaseDate, removeLastCaractere, setMoviePoster)
+        setMovieInfo(json.data)
     } catch (error) {
         searchRandomMovie()
     }
@@ -68,4 +73,5 @@ async function searchRandomMovie() {
 
 
 document.querySelector('button').addEventListener('click', searchRandomMovie)
+document.querySelector('button').addEventListener('click', updateLoadingStatus)
 
